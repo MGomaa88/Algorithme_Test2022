f3a2379fbcd8c10f21c59a78f66505647dd042a9
MGomaa88 <101111659+MGomaa88@users.noreply.github.com>
2022-10-11 12:00:44 +0200

    Exercise 4 with time complexity O(n^4)

--- Portfolio2022.cpp
+++ Portfolio2022.cpp
@@ -1,13 +1,10 @@
 // Portfolio2022.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
 #include <math.h>
 #include <string>
 
 using namespace std;
-
-
 
 //********** Excercise 1***************
 //Write a recursive algorithm/method, which takes a positive integer
@@ -16,18 +13,18 @@ using namespace std;
 int sumOfOddSquare(int N)
 {
 
-	if (N==0)
+	if (N == 0)
 	{
 		return 0;
 	}
-	else if (N%2 == 0)
+	else if (N % 2 == 0)
 	{
 
 		return sumOfOddSquare(N - 1);
 	}
 	else
 	{
-		return pow(N,2)+ sumOfOddSquare(N - 1);
+		return pow(N, 2) + sumOfOddSquare(N - 1);
 	}
 
 
@@ -40,50 +37,88 @@ int sumOfOddSquare(int N)
 bool additive(std::string s)
 {
 	int l = s.length();
-	int y = l- 1;
-	int a = l- 2;
-	int b = l-3;
+	int y = l - 1;
+	int a = l - 2;
+	int b = l - 3;
 
 	if (l == 2)
 	{
 		return false;
 	}
-	else if ( int(s.at(y)) + 48 == int( s.at(a)) + int ( s.at(b))) // the ascii value of 0 = 48, 1=49 , etc..
+	else if (int(s.at(y)) + 48 == int(s.at(a)) + int(s.at(b))) // the ascii value of 0 = 48, 1=49 , etc..
 	{
 		return true;
 	}
 	else
 	{
-		return additive(s.substr(0,l-1));
+		return additive(s.substr(0, l - 1));
 	}
 
 	return false;
 }
 
-int * getPowerOfTwo(int[])
+int* getPowerOfTwo(int input[], int length)
 {
 
-	static int myArray[3];
+	int powerTwo[10];
+	static int myArray[4];
+	for (size_t i = 0; i < sizeof powerTwo / sizeof(powerTwo[0]); i++)
+	{
+		powerTwo[i] = pow(2, i + 2);
+	}
 
-	myArray[0] = 1;
-	myArray[1] = 3;
-	myArray[2] = 5;
+
+	for (size_t first = 0; first < length - 2; first++)
+	{
 
 
-	return myArray;
+		for (size_t second = first + 1; second < length - 1; second++)
+		{
+
+
+			for (size_t third = second + 1; third < length; third++)
+			{
+
+				int tot = input[first] + input[second] + input[third];
+
+				for (size_t i = 0; i < sizeof powerTwo / sizeof(powerTwo[0]); i++)
+				{
+					/*if (powerTwo[i] < input[first] || powerTwo[i] < input[second] || powerTwo[i] < input[third])
+					{
+						i++;
+					}*/
+					 if (tot == powerTwo[i] ||  tot == powerTwo[i] - 1)
+					{
+						myArray[0] = input[first];
+						myArray[1] = input[second];
+						myArray[2] = input[third];
+						myArray[3] = powerTwo[i];
+
+						break;
+					}
+				}
+			}
+		}
+	}
+
+return myArray;
 }
 int main()
 {
-	
+
 	cout << " The sum is " << sumOfOddSquare(8) << endl;
 
 	cout << " Is there additive nr? " << additive("82842605") << endl;
 
 	int random[13] = { 23,56,22,11,65,89,3,44,87,910,45,35,98 };
 
-	int* numbers = getPowerOfTwo(random);
+	int* numbers = getPowerOfTwo(random, 13);
 
-	
+	for (size_t i = 0; i < 4; i++)
+	{
+		cout << numbers[i] << endl;
+	}
+
 
 
 }
